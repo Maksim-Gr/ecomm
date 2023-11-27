05f23b2a914c95597d43db1de7720278c01a572b
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-27 20:49:34 +0300

    chore: rename and class for model

--- src/main/java/com/maxGr/ecom/EcommApplication.java
+++ src/main/java/com/maxGr/ecom/EcommApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomApplication {
+public class EcommApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EcomApplication.class, args);
+        SpringApplication.run(EcommApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/maxGr/ecom/model/Category.java
@@ -0,0 +1,10 @@
+package com.maxGr.ecom.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="categories")
+public class Category {
+}
