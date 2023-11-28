e7a92ab39b3f409877ddca5cf7183b752ff6ef9a
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-28 22:19:10 +0300

    chore: add service && controller

---
+++ src/main/java/com/maxGr/ecom/service/CategoryService.java
@@ -0,0 +1,11 @@
+package com.maxGr.ecom.service;
+
+import com.maxGr.ecom.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
