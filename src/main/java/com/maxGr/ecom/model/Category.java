bf77ac080ce563c7ae23eeea09db8393a9c6e2b7
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-28 20:47:44 +0300

    chore: add validation && package

--- src/main/java/com/maxGr/ecom/model/Category.java
+++ src/main/java/com/maxGr/ecom/model/Category.java
@@ -1,10 +1,18 @@
 package com.maxGr.ecom.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
 
 @Entity
 @Table(name="categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+    private @NotBlank String description;
 }
