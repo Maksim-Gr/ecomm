b0d20d66343dce1c78003da7d8ee1f6fa7a16777
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-28 20:51:54 +0300

    chore: set up Category class

--- src/main/java/com/maxGr/ecom/model/Category.java
+++ src/main/java/com/maxGr/ecom/model/Category.java
@@ -15,4 +15,54 @@ public class Category {
     @Column(name = "category_name")
     private @NotBlank String categoryName;
     private @NotBlank String description;
+    private @NotBlank String imageUrl;
+
+    public Category() {
+    }
+    public Category(@NotBlank String categoryName, @NotBlank String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+    public Category(@NotBlank String categoryName, @NotBlank String description, @NotBlank String imageUrl) {
+        this.categoryName = categoryName;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "User {category id=" + id + ", category name='" + categoryName + "', description='" + description + "'}";
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
